2d6cbdec350cba0ae28df62c66ab8da5b01e9519
vjunloc <vijay.kumar01@nagarro.comm>
2014-12-25 00:24:02 +0530

    Added a dummy comment

--- grails-app/controllers/org/example/AuthorController.groovy
+++ grails-app/controllers/org/example/AuthorController.groovy
@@ -3,7 +3,8 @@ import com.wordnik.swagger.annotations.*;
 import grails.converters.JSON
 @Api(value="/author", description = "author service", produces = 'application/json,application/xml,text/html')
 class AuthorController {
-
+	
+	//Added a comment
 	@ApiOperation(httpMethod = "GET", value = "Resource to get a author")
     def index() {
 		Map indexResponse = ["message": "Hi There"] 
